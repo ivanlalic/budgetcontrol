318c76d01d54069c7c3bb40ee4dbd0ba060ff8a6
ivanlalic <ivanlalic@gmail.com>
2020-08-06 17:55:00 -0300

    Updates

---
+++ precache-manifest.07da28dad0ce70e34b7a66d8a14ec3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9630878f54cbd13b36444b5f05ac97ab",
+    "url": "/budgetcontrol/index.html"
+  },
+  {
+    "revision": "74849da1e6851ab96435",
+    "url": "/budgetcontrol/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "a8aeb8b92ed8a5a8f0b4",
+    "url": "/budgetcontrol/static/js/2.018c16d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budgetcontrol/static/js/2.018c16d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74849da1e6851ab96435",
+    "url": "/budgetcontrol/static/js/main.d32832a9.chunk.js"
+  },
+  {
+    "revision": "427091c3d4969aa3b578",
+    "url": "/budgetcontrol/static/js/runtime-main.8dd95400.js"
+  }
+]);
